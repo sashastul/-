391d7eaef506b4bc8410be86ccf3f001a6e27097
sashastul <sanec2222@gmail.com>
2016-10-16 13:32:52 +0300

    №1

---
+++ #1.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+chislo = int(input("Введите число \n"))
+summa = chislo/100 + chislo/10 - (chislo/100)*10 + (chislo - (chislo/100)*100 - (chislo/10 - (chislo/100)*10)*10)
+print(summa)
+888
